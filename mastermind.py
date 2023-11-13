bfd790a00598d58cf1c1f683aef3f81458c180dd
Fortune9077 <150080038+Fortune9077@users.noreply.github.com>
2023-11-13 20:52:12 +0000

    made a start/loading screen for a game

--- mastermind.py
+++ mastermind.py
@@ -1,91 +1,39 @@
-import random
+import pygame
+from pygame.locals import *
+import os 
+'''os.environ['SDL_AUDIODRIVER'] = 'dsp' '''
+import random 
+import math 
 
 
-def get_guess():
-    listy = []
-    
-    while len(listy) < 4:
-        q = int(input(" Guess a number: "))
-        if q < 1 or q > 7:
-            print("You can only use numbers 1-7 as guesses!")
-        elif q >= 1 and q <= 7:
-            if q not in listy:
-                listy.append(q)
-            else: 
-                print("You can only use each number once!")
-        
-    print(listy)
-    return listy
+pygame.init()
+col_1 = (255,0,0)
+col_2 = (218,165,32)
+col_3 = (0,20,168)
+
+box_width = 400
+box_height = 520
+
+screen = pygame.display.set_mode((box_width,box_height),pygame.FULLSCREEN)
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+text = font.render('python on top', True, col_1 )
+textRect = text.get_rect()
+textRect.center = (box_width// 2, box_height // 2)
 
 
+while 1:
+    screen.fill(col_3)
 
-def check_values(cl,ul):
-    listy = []
-    for i in range(4):
-        if ul[i] in cl and ul[i] != cl[i]:
-            listy.append("WHITE")
-        elif ul[i] == cl [i]:
-            listy.append("RED")
-        else: 
-            listy.append("BLACK")
-    random.shuffle(listy)
-    print(listy)
-    check_win(listy)
-    return listy
+    screen.blit(text, textRect)
+    pygame.display.update()
 
+    for event in pygame.event.get():
 
+        if event.type == pygame.QUIT:
 
-def check_win(response_list):
-    if response_list[0] == "RED" and response_list[1] == "RED" and response_list[2] == "RED" and response_list[3] == "RED":
-        print("You win!!!")
-        return "You win"
-        
-    else:
-        return "keep trying"
-    
-        
+            pygame.quit()
 
-
-
-def create_comp_list():
-    listy = []
-
-    while len(listy) < 4:
-        ran = random.randint(1,7)
-        
-        if ran not in listy:
-            listy.append(ran)
-    
-    
-    
-    return listy
-
-
-
-
-def play_game():
-    print('''Instruction: You have 5 guesses
-    If a number in the your guessed list is in the computer list, but not in the correct index, the response should be 'WHITE'.
-If a number in the your guessed list is in the computer list and in the same index, the response should be 'RED'
-If the number in the your guessed list is not in the computer list, the response should be 'BLACK'.''')
-    cl = create_comp_list()
-    guesses = 5
-    while guesses != 0:
-        inp = check_values(cl,get_guess())
-        win = check_win(inp)
-        if win == "You win":
-            break
-        guesses -= 1
-        print(f"you have {guesses} guesses left" )
-        
-        
-
-
-
-
-# Print directions telling the user how to play the game. Then call the
-# play_game function to begin the game, using all of your prewritten functions.
-
-
-
-play_game()
+            quit()
+        pygame.display.update()
