10d6d9c7e6c9ad702f46f199cc54ddec133ad621
Fortune9077 <150080038+Fortune9077@users.noreply.github.com>
2023-11-20 21:04:07 +0000

    Added one button
    Use the same steps used to make one button to make the other two buttons

--- mastermind.py
+++ mastermind.py
@@ -11,22 +11,34 @@ col_1 = (255,0,0)
 col_2 = (218,165,32)
 col_3 = (0,20,168)
 
-box_width = 400
-box_height = 520
+box_width = 500
+box_height = 400
 
 screen = pygame.display.set_mode((box_width,box_height),pygame.FULLSCREEN)
 
-font = pygame.font.Font('freesansbold.ttf', 32)
+font = pygame.font.Font('freesansbold.ttf', 48)
+font_1 = pygame.font.Font("freesansbold.ttf",24)
 
-text = font.render('python on top', True, col_1 )
-textRect = text.get_rect()
-textRect.center = (box_width// 2, box_height // 2)
+
+
+text = font.render('MASTERMIND', True, col_1 )
+texty = font_1.render('START GAME',True,col_2)
+
+
+
+def button():
+    pygame.draw.rect(screen,col_1,(250,230,220,50))
+
 
 
 while 1:
     screen.fill(col_3)
 
-    screen.blit(text, textRect)
+    screen.blit(text,(210,60))
+    button()
+    screen.blit(texty,(300,250))
+    
+    
     pygame.display.update()
 
     for event in pygame.event.get():
